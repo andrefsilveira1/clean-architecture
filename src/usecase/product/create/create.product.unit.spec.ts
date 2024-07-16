ed2988b114dd288d851675adeaa432b6f7380215
André Freitas <freitasandre38@gmail.com>
2024-07-16 10:49:46 -0300

    feat: create tests expecting return errors

--- src/usecase/product/create/create.product.unit.spec.ts
+++ src/usecase/product/create/create.product.unit.spec.ts
@@ -27,4 +27,22 @@ describe("Unit test create product use case", () => {
             price: input.price,
         });
     });
+
+    it("Should return an error until product create without name", async () => {
+        const productRepository = MockRepository();
+        const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+        input.name = "";
+
+        await expect(productCreateUseCase.execute(input)).rejects.toThrow("Name is required");
+    });
+
+    it("Should return an error until product create without price", async () => {
+        const productRepository = MockRepository();
+        const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+        input.price = -1;
+
+        await expect(productCreateUseCase.execute(input)).rejects.toThrow("Price can not be negative");
+    });
 });
