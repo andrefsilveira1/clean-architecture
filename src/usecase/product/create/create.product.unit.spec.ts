9c1faab3caeb05d32c8e936503bea52b46e880fc
André Freitas <freitasandre38@gmail.com>
2024-07-16 10:21:01 -0300

    feat: create product unit test for create use case

---
+++ src/usecase/product/create/create.product.unit.spec.ts
@@ -0,0 +1,30 @@
+
+const input = {
+    id: "123",
+    name: "product 1",
+    price: 150.00,
+}
+
+const MockRepository = () => {
+    return {
+        find: jest.fn(),
+        list: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn(),
+    }
+}
+
+describe("Unit test create product use case", () => {
+    it("Should create a product", async () => {
+        const productRepository = MockRepository();
+        const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+        const output = await productCreateUseCase.execute(input);
+
+        expect(output).toEqual({
+            id: expect.any(String),
+            name: input.name,
+            price: input.price,
+        })
+    })
+})
