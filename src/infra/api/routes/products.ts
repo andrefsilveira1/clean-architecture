f233e8b1b8e762ba588580a10563096ef175e1ac
André Freitas <freitasandre38@gmail.com>
2024-07-22 11:21:14 -0300

    feat: create product routes and register with use

--- src/infra/api/routes/products.ts
+++ src/infra/api/routes/products.ts
@@ -1,40 +1,32 @@
-// import express, {Request, Response} from "express";
-// import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
-// import CustomerRepository from "../../repository/customer";
-// import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase";
-// import CreateProductUseCase from "../../../usecase/product/create/create.product.usecase";
-// import ProductRepository from "../../repository/product";
+import express, {Request, Response} from "express";
+import CreateProductUseCase from "../../../usecase/product/create/create.product.usecase";
+import ProductRepository from "../../repository/product";
+import ListProductUseCase from "../../../usecase/product/list/list.product.usecase";
 
-// export const productRouter = express.Router();
+export const productRouter = express.Router();
 
-// productRouter.post("/", async (req: Request, res: Response) => {
-//     const usecase = new CreateProductUseCase(new ProductRepository());
+productRouter.post("/", async (req: Request, res: Response) => {
+    const usecase = new CreateProductUseCase(new ProductRepository());
 
-//     try {
-//         const customerDto = {
-//             name: req.body.name,
-//             address: {
-//                 street: req.body.address.street,
-//                 city: req.body.address.city,
-//                 number: req.body.address.number,
-//                 zip: req.body.address.zip,
-//             }
-//         }
+    try {
+        const customerDto = {
+            name: req.body.name,
+            price: req.body.price
+        }
+        const output = await usecase.execute(customerDto);
+        res.send(output);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
-//         const output = await usecase.execute(customerDto);
-//         res.send(output);
-//     } catch (err) {
-//         res.status(500).send(err);
-//     }
-// });
+productRouter.get("/", async (req: Request, res: Response) => {
+    const usecase = new ListProductUseCase(new ProductRepository());
 
-// productRouter.get("/", async (req: Request, res: Response) => {
-//     const usecase = new ListCustomerUseCase(new CustomerRepository());
-
-//     try {
-//         const output = await usecase.execute({});
-//         res.send(output);
-//     } catch (err) {
-//         res.status(500).send(err);
-//     }
-// });
+    try {
+        const output = await usecase.execute({});
+        res.send(output);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
