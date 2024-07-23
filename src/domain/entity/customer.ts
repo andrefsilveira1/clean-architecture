45ef9339d68a5e680c2483bc21736373a584d126
André Freitas <freitasandre38@gmail.com>
2024-07-23 10:12:06 -0300

    refactor: change abstract class to fit get id

--- src/domain/entity/customer.ts
+++ src/domain/entity/customer.ts
@@ -10,7 +10,7 @@ export default class Customer extends Entity{
 
     constructor(id: string, name: string) {
         super();
-        this.id = id,
+        this._id = id,
         this._name = name,
         this.validate();
 
@@ -20,7 +20,7 @@ export default class Customer extends Entity{
     }
 
     validate() {
-        if(this.id.length === 0) {
+        if(this._id.length === 0) {
             this.notification.addError({
                 context: "Customer",
                 message: "Id is required"
